bfcc7893781fa4e4c0853473f6cb7ef029160bc4
Dipanshu <dipanshu18881@gmail.com>
2024-03-03 16:29:15 +0530

    Update 1

--- src/test/java/com/enterpriseproject/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/enterpriseproject/productservice/ProductServiceApplicationTests.java
@@ -1,21 +1,29 @@
 package com.enterpriseproject.productservice;
 
-//@SpringBootTest
-//class ProductServiceApplicationTests {
-//
-//    @Autowired
-//    private ProductRepository productRepository;
-//
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//    @Test
-//    @Transactional
-//    @Commit //  Required as delete query needs to update the database
-//    void testQueries() {
-//        productRepository.findByTitleContaining("dipanshu");
-//        productRepository.deleteByTitle("dipanshu");
-//    }
-//}
+import com.enterpriseproject.productservice.Repositories.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    @Transactional
+    @Commit
+        //  Required as delete query needs to update the database
+    void testQueries() {
+        productRepository.findByTitleContaining("dipanshu");
+        productRepository.deleteByTitle("dipanshu");
+    }
+}
